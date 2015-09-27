64d8bf20a04bb022d4f389fff6e3021f3a853e2d
limeng32 <limeng32@live.cn>
2015-09-27 17:04:26 +0800

    Do some change 2.

--- src/article/article.js
+++ src/article/article.js
@@ -4,7 +4,7 @@ var XTemplate = require("kg/xtemplate/3.3.3/runtime");
 var addition = require('../addition/addition');
 module.exports = {
     init: function () {
-        S.log('article init111');
+        S.log('article init1112');
         var html = new XTemplate(tpl).render({
             title: 'this is article' + addition.run(4, 3),
             content: 'render by kg/xtemplate'
